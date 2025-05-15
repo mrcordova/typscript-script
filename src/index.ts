ae7f3154051dcef4cc3416a8ae646cae648c4582
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-05-14 18:41:59 -0700

    add app path

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import path from "path";
 
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", (_, res) => {
+  res.set("Content-Type", "text/plain;charset=utf-8");
+  res.status(200).send("OK");
+  res.end();
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
