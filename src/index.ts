c02027b7367e23dd4d3cfedba301d3cc5b1753d3
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-06-29 20:44:53 -0700

    add middleware function for statuscode

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,31 @@ import path from "path";
 
 const app = express();
 const PORT = 8080;
-app.use("/app", express.static("./src/app"));
 
-app.get("/healthz", (_, res) => {
+function middlewareLogResponses(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void {
+  res.on("finish", () => {
+    if (res.statusCode < 200 || res.statusCode >= 300) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+    }
+  });
+  next();
+}
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+
+app.get("/healthz", (_, res: express.Response) => {
   res.set("Content-Type", "text/plain;charset=utf-8");
   res.status(200).send("OK");
   res.end();
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, (e) => {
+  console.log(e);
   console.log(`Server is running at http://localhost:${PORT}`);
 });
